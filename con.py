13126634266043bdf692b7f3ce268fa75f5f055b
alim <alim.kidh11@gmail.com>
2019-03-26 22:19:16 +0700

    sql update make query

--- con.py
+++ con.py
@@ -9,16 +9,24 @@ port = '3306'
 database = 'db_log'
 table = 'tb_test2'
 
-try:
-    engine = create_engine('mysql+mysqlconnector://'+username+':'+password+'@'+host+':'+port+'/'+database+'?use_unicode=true')
-    con = engine.connect()
-    connected = True
-except:
-    print('Error: Koneksi ke DB gagal!')
-    connected = False
-if (connected):
-    df = pd.read_sql(table, con=con)
-    df = df.drop_duplicates(subset=['shortcode'],keep='first')
-    df.to_sql(name=table ,con=con,if_exists='replace')
-    
-    
+class sql_con():
+    def __init__(self, table):
+        self.table = table
+        try:
+            engine = create_engine('mysql+mysqlconnector://'+username+':'+password+'@'+host+':'+port+'/'+database+'?use_unicode=true')
+            self.con = engine.connect()
+            self.connected = True
+        except:
+            # print('Error: Koneksi ke DB gagal!')
+            self.connected = False
+        self.df = pd.read_sql(self.table, con=con)
+    def deduplicate(self, fieldname): #fieldname =['shortcode']
+        if (self.connected):
+            self.df = self.df.drop_duplicates(subset=fieldname,keep='first')
+            self.df.to_sql(name=self.table ,con=con,if_exists='replace')
+    def add_data(self, data):
+        self.df.append(data)
+        self.df.to_sql(name=self.table ,con=con,if_exists='replace')
+    def make_csv(self, name):
+        self.df.to_csv(name, sep=",", index=False)
+        print('Saved as', str(name))
