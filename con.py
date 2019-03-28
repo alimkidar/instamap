13ba65297ebc82c630c3ff43961afa76753b0a04
alim <alim.kidh11@gmail.com>
2019-03-28 22:36:01 +0700

    fix all problems

--- con.py
+++ con.py
@@ -15,6 +15,7 @@ class sql_con():
             engine = create_engine('mysql+mysqlconnector://'+username+':'+password+'@'+host+':'+port+'/'+database+'?use_unicode=true')
             self.con = engine.connect()
             self.connected = True
+            print('Koneksi berhasil', table)
         except:
             print('Error: Koneksi ke DB gagal!')
 
@@ -24,15 +25,20 @@ class sql_con():
             shortcode = i['shortcode']
             try:
                 dfc = pd.read_sql_query("""SELECT * FROM """ + self.table + """ WHERE shortcode = '""" + shortcode + "'", con=self.con)
-                if len(dfc)!=0:
+                if len(dfc)==0:
                     bucket_new.append(i)
+                else:
+                    print(shortcode, 'ada')
             except:
                 bucket_new.append(i)
         if len(bucket_new) != 0:
             df_new = pd.DataFrame(bucket_new)
-            print(df_new)
+            # print(df_new)
             df_new.to_sql(self.table,con=self.con, if_exists='append')
+        print('table:', self.table, str(len(bucket_new)))
 
     def get_df(self):
         df = pd.read_sql(self.table, con=self.con)
         return df
+    def close(self):
+        self.con.close()
--- instamap.py
+++ instamap.py
@@ -1,7 +1,17 @@
 import pandas as pd
-import requests, json, time
+import requests, json, time, unicodedata
+from con import sql_con
 
 headers = "User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0"
+def deEmojify(inputString):
+    returnString = ""
+    for character in inputString:
+        try:
+            character.encode("ascii")
+            returnString += character
+        except UnicodeEncodeError:
+            returnString += ''
+    return returnString
 
 def load_json(link, headers):
     flag = False
@@ -27,30 +37,40 @@ def load_json(link, headers):
     else:
         x = 0
     return x
-def explore_hashtag(tag):
-    bucket = []
+def explore_hashtag(tag, bucket_dirty, bucket_clean):
     url_tags = 'https://www.instagram.com/explore/tags/'+tag+'/?__a=1'
     data_tags = load_json(url_tags, headers)
-    edges = data['graphql']['hashtag']['edge_hashtag_to_media']['edges']
+    edges = data_tags['graphql']['hashtag']['edge_hashtag_to_media']['edges']
     for edge in edges:
         shortcode = edge['node']['shortcode']
-        url_sc = 'https://www.instagram.com/p/'+sc+'/?__a=1'
+        print(shortcode)
+        url_sc = 'https://www.instagram.com/p/'+shortcode+'/?__a=1'
         data_sc = load_json(url_sc, headers)
         lokasi = data_sc['graphql']['shortcode_media']['location']
         if (lokasi):
-            url_loc = 'https://www.instagram.com/explore/locations/'+lokasi+'/?__a=1'
-            data_loc = load_json(url_loc, headers)
-            open_post(data_loc, bucket)
+            data = open_post(data_sc)
+            bucket_clean.append(data[0])
+        bucket_dirty.append({'hashtag':tag, 'shortcode':shortcode})
 
 def classifier(accessibility_caption): # TODO: buat klasifikasi foto
     string = accessibility_caption.lower()
-    if 'mountain' in string:
-        x = 'mountain'
-def open_post(data, bucket):
-    root = data['graphql']['shortcode_media']
+    cluster = ['mountain', 'people', 'beach', 'ocean']
+    x = 'other'
+    for i in cluster:
+        if i in string:
+            x = i
+    return x
+
+def open_post(data):
+    bucket = []
+    try:
+        root = data['graphql']['shortcode_media']
+    except:
+        # print(data)
+        return print("error:")
     username = root['owner']['username']
     display_url = root['display_url']
-    hight = root['dimensions']['hight']
+    height = root['dimensions']['height']
     width = root['dimensions']['width']
     shortcode = root['shortcode']
     try:
@@ -60,10 +80,10 @@ def open_post(data, bucket):
     timestamp = root['taken_at_timestamp']
     type_post = root['__typename']
     if type_post=='GraphImage':
-        accessibility_caption = ['accessibility_caption']
+        accessibility_caption = root['accessibility_caption']
     else:
         accessibility_caption = ''
-
+    cluster = classifier(accessibility_caption)
     if (root['location']):
         location_id = str(root['location']['id'])
         location_name = root['location']['name']
@@ -75,17 +95,38 @@ def open_post(data, bucket):
         bucket.append({
             'username':username,
             'display_url': display_url,
-            'hight': hight,
+            'height': height,
             'width': width,
             'shortcode':shortcode,
-            'conversation': conversation,
+            'conversation': deEmojify(conversation),
             'timestamp': timestamp,
             'accessibility_caption': accessibility_caption,
             'location_id':location_id,
-            'location_name':location_name,
+            'location_name':deEmojify(location_name),
             'lat': lat,
             'lng': lng,
             'count':count,
             'url_loc': 'https://www.instagram.com/explore/locations/'+location_id+'/',
-            'url_post': 'https://www.instagram.com/p/'+shortcode+'/'
+            'url_post': 'https://www.instagram.com/p/'+shortcode+'/',
+            'cluster':cluster
         })
+    return bucket
+bucket_clean = []
+bucket_dirty = []
+
+print('req')
+explore_hashtag('exploreindonesia', bucket_dirty, bucket_clean)
+print('req success')
+
+
+tb_clean = sql_con('tb_clean1')
+tb_clean.add_data(bucket_clean)
+df = tb_clean.get_df()
+
+tb_dirty = sql_con('tb_dirty1')
+tb_dirty.add_data(bucket_dirty)
+
+df['latlng'] = df['lat'] + df['lng']
+df = df.drop_duplicates(subset=['latlng'], keep='last')
+df.to_csv('data.csv',index=False)
+print('done')
